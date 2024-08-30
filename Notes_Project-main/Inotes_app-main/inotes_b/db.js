877b8ba0fce221b2b720f5e66cfa970162912fc5
Sushanta Sarkar <108208255+SushantaSarkar123@users.noreply.github.com>
2024-08-30 13:36:11 +0530

    Update db.js

--- Notes_Project-main/Inotes_app-main/inotes_b/db.js
+++ Notes_Project-main/Inotes_app-main/inotes_b/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongooseURI = 'mongodb+srv://nandkuliyar1:AqdpP2FWQI4oN2Gv@cluster0.wf8x6xi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongooseURI = 'mongodb+srv://<db_username>:<db_password>@<cluster-address>/test?retryWrites=true&w=majority';
 
 const connectToMongoose = async () =>{
     await mongoose.connect(mongooseURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
