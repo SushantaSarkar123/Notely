90a5ad1fa0d5d9a036836038e5f55b6fa33850bb
Sushanta Sarkar <108208255+SushantaSarkar123@users.noreply.github.com>
2025-03-14 09:58:19 +0530

    Removed old project from main (Backup in old-version)

--- Notes_Project-main/Inotes_app-main/inotes_b/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongooseURI = 'mongodb+srv://<db_username>:<db_password>@<cluster-address>/test?retryWrites=true&w=majority';
-
-const connectToMongoose = async () =>{
-    await mongoose.connect(mongooseURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
-}
-
-module.exports = connectToMongoose;
